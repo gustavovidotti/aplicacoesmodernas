838be5ef9faf2fbce8df5620142caff1b2f0f772
Gustavo Vidotti <gustavo.vidotti@gmail.com>
2018-04-28 10:15:41 -0300

    Autenticacao com JWT

--- Vidotti.Api/Startup.cs
+++ Vidotti.Api/Startup.cs
@@ -1,11 +1,17 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc.Authorization;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.IdentityModel.Tokens;
+using Vidotti.Api.Security;
 using Vidotti.Domain.Commands.Handlers;
 using Vidotti.Domain.Repositories;
 using Vidotti.Domain.Services;
@@ -18,12 +24,86 @@ namespace Vidotti.Api
 {
     public class Startup
     {
+        //https://auth0.com/blog/securing-asp-dot-net-core-2-applications-with-jwts/
+
+        private const string ISSUER = "B0FD3632";
+        private const string AUDIENCE = "B5C8935F2620";
+        private const string SECRET_KEY = "3418C3C4-1CE1-4107-A641-7451D078B62E";
+
+        private readonly SymmetricSecurityKey _signingKey = new SymmetricSecurityKey(Encoding.ASCII.GetBytes(SECRET_KEY));
+
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc();
+            services.AddMvc(config =>
+            {
+                var policy = new AuthorizationPolicyBuilder()
+                                 .RequireAuthenticatedUser()
+                                 .Build();
+                config.Filters.Add(new AuthorizeFilter(policy));
+            });
             services.AddCors();
+
+            var tokenValidationParameters = new TokenValidationParameters
+            {
+                ValidateIssuer = true,
+                ValidIssuer = ISSUER,
+
+                ValidateAudience = true,
+                ValidAudience = AUDIENCE,
+
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = _signingKey,
+
+                RequireExpirationTime = true,
+                ValidateLifetime = true,
+
+                ClockSkew = TimeSpan.Zero
+            };
+
+            services.AddAuthentication(authOptions =>
+                {
+                    authOptions.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                    authOptions.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                })
+                .AddJwtBearer(jwt =>
+                {
+                    jwt.TokenValidationParameters = tokenValidationParameters;
+
+                    jwt.Events = new JwtBearerEvents
+                    {
+                        OnAuthenticationFailed = context =>
+                        {
+                            Console.WriteLine("Token inválido..:. " + context.Exception.Message);
+                            return Task.CompletedTask;
+                        },
+                        OnTokenValidated = context =>
+                        {
+                            Console.WriteLine("Toekn válido...: " + context.SecurityToken);
+                            return Task.CompletedTask;
+                        }
+                    };
+                });
+
+            services.AddAuthorization(options =>
+            {
+                options.AddPolicy("User", policy => policy.RequireClaim("VidottiStore", "User"));
+                options.AddPolicy("Admin", policy => policy.RequireClaim("VidottiStore", "Admin"));
+                options.AddPolicy("Bearer", new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme).RequireAuthenticatedUser().Build());
+            });
+
+            services.Configure<TokenOptions>(options =>
+            {
+                options.Issuer = ISSUER;
+                options.Audience = AUDIENCE;
+                options.SigningCredentials = new SigningCredentials(_signingKey, SecurityAlgorithms.HmacSha256);
+            });
+
+            services.AddAuthorization(options =>
+            {
+                options.DefaultPolicy = new AuthorizationPolicyBuilder(JwtBearerDefaults.AuthenticationScheme).RequireAuthenticatedUser().Build();
+            });
 
             services.AddScoped<VidottiDataContext, VidottiDataContext>(); //Gera uma instacia por contexto
             services.AddTransient<IUow, Uow>(); //Gera uma nova interface por request
@@ -52,6 +132,8 @@ namespace Vidotti.Api
                 x.AllowAnyMethod();
                 x.AllowAnyOrigin();
             });
+
+            app.UseAuthentication();
             app.UseMvc();
         }
     }
